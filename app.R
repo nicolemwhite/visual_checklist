a3d9bca1ca1d76fbb00052b72e65bdec37b3a678
Nicole <nm.white@qut.edu.au>
2023-01-09 12:53:59 +1000

    update menu visibility

--- app.R
+++ app.R
@@ -21,61 +21,52 @@ legend_positions <- list("No" = 'none',"Yes" = 'top')
 
 
 header<- dashboardHeader(title = "Visual reporting checklists", disable = FALSE, titleWidth  = 600)
-  ## Sidebar content
-
-sidebar<- dashboardSidebar(
-    useShinyjs(),
-    width=600,
-    sidebarMenu(
-      id = 'sidebar',
-      menuItem("Application", tabName = "setup", icon = icon("list"),startExpanded = F),
-      menuItem("Customisation", tabName = "customise", icon = icon("wrench"),startExpanded=T),
-      
-      div(id = 'sidebar_setup',
-          conditionalPanel("input.sidebar == 'setup'",
-                           fluidRow(
-                             column(7,selectInput(inputId="template",label='Select template (TODO)',choices=c("CHEERS","STROBE","TRIPOD","PROBAST",selected="CHEERS"))),
-                             column(4,downloadButton("download_template"),style = 'margin-top:35px')),
-                           fileInput("upload", "Upload completed template (.csv)", accept = c(".csv"))
-          ),
-          
-          conditionalPanel("input.sidebar == 'customise'",
-                           fluidRow(style='margin-left:0px',
-                                    column(6,selectInput('colourscheme',label='Choose colour scheme',choices = names(colourschemes),selected = 'Greyscale')),
-                                    column(6, selectInput("theme", label = "Select plot theme", choices = names(themes),selected = 'Minimal'))),
-                           fluidRow(style='margin-left:0px',column(6,textInput('xlabtext',label='x-axis label',value = 'Value')),
-                                    column(6,textInput('ylabtext',label='y-axis label',value = 'Density'))),
-                           fluidRow(style='margin-left:0px',column(6,radioButtons(inputId='legend','Display legend?',choices=names(legend_positions),selected = "Yes",inline=TRUE),style = 'margin-top:10px'),column(6,uiOutput("legend_title"))),
-                           fluidRow(style='margin-left:0px',
-                                    column(3,selectInput("fformat", "Format",c("png" = "png","tiff" = "tiff","jpeg" = "jpeg"), 'png')),
-                                    column(3,selectInput(inputId = "fres",label = "Resolution",c("300 dpi"=300,"600 dpi"=600),selected = "300")),
-                                    column(3,numericInput(inputId = "fheight",label = "Height (cm)",min = 8,max = 22,step = 1,value = 10)),
-                                    column(3,numericInput(inputId = "fwidth",label = "Width (cm)",min = 8,max = 22,step = 1,value = 15))),
-                           fluidRow(style='margin-left:0px',column(10,textInput(inputId = 'fig_fname',label='Figure name',value='figure'))),
-                           fluidRow(column(6,downloadButton("downloadFigure", "Download Figure",style = 'margin-left:30px;background-color:	#f9f9f9;font-family: Arial;font-weight: bold')))
-          ),
-          ),
-      
-      menuItem("References", tabName = "citation", icon = icon("list-alt"))
-    )
+## Sidebar content
+sidebar <- dashboardSidebar(
+  width=600,
+  sidebarMenu(id = "sidebar",
+              menuItem("Application", tabName = "setup", icon = icon("list"),startExpanded = T),
+              menuItem("Customisation", tabName = "customise", icon = icon("wrench"),startExpanded=F),
+              menuItem("References", tabName = "citation", icon = icon("list-alt")),
+              conditionalPanel(condition="input.sidebar == 'setup'",
+                               fluidRow(
+                                 column(7,selectInput(inputId="template",label='Select template (TODO)',choices=c("CHEERS","STROBE","TRIPOD","PROBAST",selected="CHEERS"))),
+                                 column(4,downloadButton("download_template"),style = 'margin-top:35px')),
+                               fileInput("upload", "Upload completed template (.csv)", accept = c(".csv"))),
+              
+              conditionalPanel(condition="input.sidebar == 'customise'",
+                               fluidRow(style='margin-left:0px',
+                                        column(6,selectInput('colourscheme',label='Choose colour scheme (todo)',choices = names(colourschemes),selected = 'Greyscale')),
+                                        column(6, radioButtons(inputId='legend','Display legend?',choices=names(legend_positions),selected = "Yes",inline=TRUE))),
+                               fluidRow(style='margin-left:0px',column(6,textInput('xlabtext',label='x-axis label (todo)',value = 'todo')),
+                                        column(6,textInput('ylabtext',label='y-axis label (todo)',value = 'todo')))
+              )
   )
-  ## Body content
+)
+
+## Body content
 body <- dashboardBody(
-    
-    tabItems(
-      # main output window
-      tabItem(tabName = "setup",
-              column(2,selectInput(inputId='chooseViz',label='Select plot',choices=c("Full dataset","Summary by study","Summary by checklist item"),selected="Full dataset",multiple = F)),
+  
+  tabItems(
+    # main output window
+    tabItem(tabName = "setup",
+            fluidRow(
+              column(3,selectInput(inputId='chooseViz',label='Select plot',choices=c("Full dataset","Summary by study","Summary by checklist item"),selected="Full dataset",multiple = F)),
               downloadButton("downloadFigure", "Download",style = 'margin-left:0px;margin-top:25px;background-color:	#f9f9f9;font-family: Arial;font-weight: bold'),
-              fluidRow(box(title=NULL,solidHeader=T,width=12,column(12,align="center",plotOutput("plot1",width = "1000px",height = "800px"))))
-      ),
-      #citation info
-      tabItem(tabName = 'citation',h3('References'))
+              column(1,selectInput("fformat", "Format",c("png" = "png","tiff" = "tiff","jpeg" = "jpeg"), 'png')),
+              column(2,selectInput(inputId = "fres",label = "Resolution (dpi)",c("300 dpi"=300,"600 dpi"=600),selected = "300")),
+              column(1,numericInput(inputId = "fheight",label = "Height (in)",min = 1, value = 10)),
+              column(1,numericInput(inputId = "fwidth",label = "Width (in)",min = 1,value = 15))
+            ),
+            fluidRow(box(title=NULL,solidHeader=T,width=12,column(12,align="center",plotOutput("plot1",width = "1000px",height = "800px"))))
     ),
-    
-    
-    #style elements
-    tags$head(tags$style(HTML('
+    #citation info
+    tabItem(tabName = 'citation',h3('References'))
+  ),
+  
+  
+  #style elements
+  tags$head(tags$style(HTML('
                                 /* logo */
                                 .skin-blue .main-header .logo {
                                 background-color: #003D58;
@@ -122,42 +113,44 @@ body <- dashboardBody(
                                 /*other text*/
                                 .shiny-output-error-validation {color: #EAE23B;font-weight: bold;}
                                 ')))
-    
-    
-  )
   
+  
+)
 
-  
+
+
 ui <- dashboardPage(header, sidebar, body)
-  
-server <- function(input, output,session) {
 
+server <- function(input, output,session) {
+  
   set.seed(122)
   data <- reactive({
     req(input$upload)
-
+    
     ext <- tools::file_ext(input$upload$name)
     switch(ext,
            csv = vroom::vroom(input$upload$datapath, delim = ",",col_types="c"),
            tsv = vroom::vroom(input$upload$datapath, delim = "\t"),
            validate("Invalid file; Please upload a .csv or .tsv file")
     )
-
+    
   })
-
+  
   output$head <- renderTable(data())
-
-
+  
+  #need to hardcode item order|checklist
   plot_studies <- function(){
+    show_legend = legend_positions[[input$legend]]
+    
     plot_data = data() %>% mutate(author_year = paste(author,year)) %>% select(-author,-year) %>%
       gather(item,item_score,-author_year) %>% mutate_at('item_score',~replace_na(.,'missing'))
     if(input$chooseViz=="Full dataset"){
-      final_plot <- plot_data %>% ggplot(aes(x=item,y=author_year,fill=item_score))+
+      final_plot <- plot_data %>% ggplot(aes(x=str_wrap(item,30),y=author_year,fill=item_score))+
         geom_tile(colour = 'white', size = 0.5) +
         theme(axis.text.x = element_text(angle = 45, hjust=1),
               panel.background = element_blank(),
-              text = element_text(size=12),
-              legend.title = element_blank(),legend.position = 'top',legend.direction = 'horizontal') +
+              text = element_text(size=14),
+              legend.title = element_blank(),legend.position = show_legend,legend.direction = 'horizontal') +
         labs(x = 'Checklist item', y = 'Study')
     }
     if(input$chooseViz=="Summary by study"){
@@ -165,25 +158,60 @@ server <- function(input, output,session) {
       final_plot <- plot_data %>% ggplot(aes(y=author_year,fill=item_score))+geom_bar()+
         scale_x_continuous('Number of items',breaks=0:n_items)+
         theme(panel.background = element_blank(),
-              text = element_text(size=12),
+              text = element_text(size=14),
               legend.title = element_blank(),legend.position = 'top',legend.direction = 'horizontal')+
         labs(y='Study')
-
+      
     }
     if(input$chooseViz=="Summary by checklist item"){
       n_studies = length(unique(plot_data$author_year))
-      final_plot <- plot_data %>% ggplot(aes(y=item,fill=item_score))+geom_bar()+
+      final_plot <- plot_data %>% ggplot(aes(y=str_wrap(item,30),fill=item_score))+geom_bar()+
         scale_x_continuous('Number of studies',breaks=0:n_studies)+
         theme(panel.background = element_blank(),
-              text = element_text(size=12),
+              text = element_text(size=14),
               legend.title = element_blank(),legend.position = 'top',legend.direction = 'horizontal')+
         labs(y='Checklist item')
     }
     final_plot <- final_plot +scale_fill_manual(values=cbPalette)
     return(final_plot)
   }
-
+  
   output$plot1 <- renderPlot({plot_studies()})
-
+  
+  #figure
+  fn_download_fig <- function()
+  {
+    fheight <- input$fheight
+    fwidth <- input$fwidth
+    fres <- as.numeric(input$fres)
+    
+    # open file dependent on format    
+    if(input$fformat=="png") png(fn_downloadname_fig(), height=fheight, width=fwidth, res=fres, units="in")
+    if(input$fformat=="tiff") tiff(fn_downloadname_fig(), height=fheight, width=fwidth, res=fres, units="in", compression="lzw")
+    if(input$fformat=="jpeg") jpeg(fn_downloadname_fig(), height=fheight, width=fwidth, res=fres, units="in", quality=100)
+    
+    g = plot_studies()
+    print(g)
+    dev.off()
+  }
+  # create filename
+  fn_downloadname_fig <- reactive({
+    
+    fname = "Figure" #isolate(input$fig_fname)
+    if(input$fformat=="png") filename <- paste0(fname,".png",sep="")
+    if(input$fformat=="tiff") filename <- paste0(fname,".tif",sep="")
+    if(input$fformat=="jpeg") filename <- paste0(fname,".jpg",sep="")
+    return(filename)
+  })
+  
+  # download handler
+  output$downloadFigure <- downloadHandler(
+    filename = fn_downloadname_fig,
+    content = function(file) {
+      fn_download_fig()
+      file.copy(fn_downloadname_fig(), file, overwrite=T)
+    }
+  )  
+  
 }
 shinyApp(ui, server)
